fd22fba73747c37278fcd37919e00701b4d2e996
shashangka shekhar <shashangka@live.com>
2016-01-26 10:15:37 +0600

    MVC5-SignalR

---
+++ WebAppSignalR/Hubs/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebAppSignalR.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public void SayHello(String message)
+        {
+            Clients.All.Hello(message);
+        }
+    }
+}
